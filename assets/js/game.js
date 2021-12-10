1fc78bba11224595731b9a5a51aa88805e2e4c3c
Ricky27 <Rickyrenteria7@gmail.com>
2021-12-09 16:53:53 -0800

    added console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JS is running!");
+
+   var playername = window.prompt("What is your robots names?");
+  console.log(playername);
+
+  console.log("This logs a string, good for leaving a message");
+  console.log(10 + 10);
+  console.log("Our robot's name is " + playername);
+
+    function fight() {
+        window.alert("The Fight has begun bastards!");
+    }
+
+    //fight();
